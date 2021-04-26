bc63b9c2de55f02cd4b6b4b58f285213e83b2eac
Phạm Đức Bình <binh1298@gmail.com>
2021-04-26 21:41:29 +0700

    chore: fix eslint prettier error

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import React from "react";
 
-import { Header, HeaderProps } from './Header';
+import { Header, HeaderProps } from "./Header";
+
+import { Story, Meta } from "@storybook/react";
 
 export default {
-  title: 'Example/Header',
-  component: Header,
+    title: "Example/Header",
+    component: Header,
 } as Meta;
 
 const Template: Story<HeaderProps> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  user: {},
+    user: {},
 };
 
 export const LoggedOut = Template.bind({});
