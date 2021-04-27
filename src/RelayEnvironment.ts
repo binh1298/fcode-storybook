00f118aafaf6601df16a3b4b597691daaec9cd0a
Phạm Đức Bình <binh1298@gmail.com>
2021-04-27 23:43:32 +0700

    fix: add typings for relay utils

--- src/RelayEnvironment.ts
+++ src/RelayEnvironment.ts
@@ -1,13 +1,14 @@
-import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import { Environment, FetchFunction, Network, RecordSource, Store } from "relay-runtime";
 
 import fetchGraphQL from "./fetchGraphQL";
 
 // Relay passes a "params" object with the query name and text. So we define a helper function
 // to call our fetchGraphQL utility with params.text.
-async function fetchRelay(params, variables) {
+const fetchRelay: FetchFunction = async (params, variables) => {
+    // eslint-disable-next-line no-console
     console.log(`fetching query ${params.name} with ${JSON.stringify(variables)}`);
     return fetchGraphQL(params.text, variables);
-}
+};
 
 // Export a singleton instance of Relay Environment configured with our network function:
 export default new Environment({
--- src/fetchGraphQL.ts
+++ src/fetchGraphQL.ts
@@ -1,6 +1,9 @@
-async function fetchGraphQL(text, variables) {
+import { Variables } from "relay-runtime";
+
+async function fetchGraphQL(text: string | null | undefined, variables: Variables) {
     const REACT_APP_HASURA_ADMIN_SECRET = process.env.REACT_APP_HASURA_ADMIN_SECRET;
     const REACT_APP_HASURA_END_POINT = process.env.REACT_APP_HASURA_END_POINT;
+    if (!REACT_APP_HASURA_END_POINT) throw new Error("Missing environment");
     const response = await fetch(REACT_APP_HASURA_END_POINT, {
         method: "POST",
         headers: {
