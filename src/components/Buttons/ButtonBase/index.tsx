b497d0e4ad3a32fdcae7646f27964629a7a2597b
Thien Duc <thienduc@Thiens-MacBook-Pro.local>
2021-04-28 23:32:01 +0700

    change borderRadius value

--- src/components/Buttons/ButtonBase/index.tsx
+++ src/components/Buttons/ButtonBase/index.tsx
@@ -1,10 +1,9 @@
 import { Button as MaterialButton, ButtonProps as MaterialButtonProps } from "@material-ui/core";
-import { Add as AddIcon, Edit as EditIcon } from "@material-ui/icons";
 
 export interface ButtonBaseProps extends MaterialButtonProps {}
 
 const buttonStyle = {
-    borderRadius: "50em",
+    borderRadius: "2em",
 };
 
 const ButtonBase = (props: ButtonBaseProps) => {
