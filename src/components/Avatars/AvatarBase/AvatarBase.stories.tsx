631d5ca057089432bb4c42cd3fae756eea9f8060
Trung Kien <kien123456k@gmail.com>
2021-05-06 00:30:15 +0700

    update Avatar Image link

--- src/components/Avatars/AvatarBase/AvatarBase.stories.tsx
+++ src/components/Avatars/AvatarBase/AvatarBase.stories.tsx
@@ -12,5 +12,5 @@ const Template: Story<AvatarBaseProps> = (args) => <AvatarBase {...args} />;
 export const ImageAvatar = Template.bind({});
 ImageAvatar.args = {
     alt: "Fcode avatar",
-    src: "https://res.cloudinary.com/dq7l8216n/image/upload/v1620214495/Fcodelogo.png",
+    src: "https://res.cloudinary.com/dq7l8216n/image/upload/v1620235303/FCode-Avatar.png",
 };
