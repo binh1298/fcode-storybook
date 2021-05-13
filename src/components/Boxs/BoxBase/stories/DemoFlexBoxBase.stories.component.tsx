0031c173ba9d00088f497b857452dd141b119020
Binh Truong <binhtt.13122000@gmail.com>
2021-05-13 22:30:53 +0700

    add typography option to stories

--- src/components/Boxs/BoxBase/stories/DemoFlexBoxBase.stories.component.tsx
+++ src/components/Boxs/BoxBase/stories/DemoFlexBoxBase.stories.component.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import BoxBase, { BoxBaseProps } from "..";
 import AvatarBase from "../../../Avatars/AvatarBase";
+import ButtonBase from "../../../Buttons/ButtonBase";
+import TypographyBase from "../../../Typography/TypographyBase";
 
 const source = "https://res.cloudinary.com/dq7l8216n/image/upload/v1620235303/FCode-Avatar.png";
 
-const ChildrenComponentForFlexStory = () => {
+type DemoType = "typography" | "avatar";
+
+const ChildrenComponentForFlexStory = ({ type }: { type: DemoType }) => {
     const data: Array<number> = [1, 2, 3];
     return (
         <React.Fragment>
             {data.map((item) => {
                 return (
                     <BoxBase m={1} p={1} key={item}>
-                        <AvatarBase size="large" src={source} />
+                        {type === "avatar" ? (
+                            <AvatarBase size="large" src={source} />
+                        ) : (
+                            <TypographyBase variant="h3">Item {item}</TypographyBase>
+                        )}
                     </BoxBase>
                 );
             })}
@@ -21,9 +29,18 @@ const ChildrenComponentForFlexStory = () => {
 };
 
 const DemoFlexBoxBase = (props: BoxBaseProps) => {
+    const [type, setType] = useState<DemoType>("avatar");
+    const changeTypeDemoHandler = () => {
+        type === "avatar" ? setType("typography") : setType("avatar");
+    };
     return (
-        <BoxBase {...props}>
-            <ChildrenComponentForFlexStory />
+        <BoxBase>
+            <BoxBase {...props}>
+                <ChildrenComponentForFlexStory type={type} />
+            </BoxBase>
+            <ButtonBase variant="contained" color="primary" onClick={changeTypeDemoHandler}>
+                Demo by {type === "avatar" ? "typography" : "avatar"}
+            </ButtonBase>
         </BoxBase>
     );
 };
