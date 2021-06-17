9a8c7fd061762632f299bf63ab72a364c8658fae
Trung Kien <kien123456k@gmail.com>
2021-06-17 18:26:11 +0700

    add Divider Base stories

--- src/components/Lists/ListBase/index.tsx
+++ src/components/Lists/ListBase/index.tsx
@@ -5,11 +5,11 @@ import BoxBase from "../../Boxs/BoxBase";
 export interface ListBaseProps extends MaterialListProps {}
 
 const ListBase = (props: ListBaseProps) => {
-    const { ...rest } = props;
+    const { disablePadding } = props;
 
     return (
-        <BoxBase bgcolor="primary" px={2}>
-            <MaterialList {...rest} />
+        <BoxBase bgcolor="primary" px={disablePadding ? 0 : 2}>
+            <MaterialList {...props} />
         </BoxBase>
     );
 };
