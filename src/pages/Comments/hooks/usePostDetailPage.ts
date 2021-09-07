a679c48fb4b3a35569b6c1e42cdde94153ce7058
Trung Kien <kien123456k@gmail.com>
2021-09-07 10:59:16 +0700

    remove option retry from usePostDetailPage hook

--- src/pages/Comments/hooks/usePostDetailPage.ts
+++ src/pages/Comments/hooks/usePostDetailPage.ts
@@ -7,36 +7,30 @@ import useQueryClient from "src/hooks/useQueryClient";
 const usePostDetailPage = (postId: string) => {
     const queryClient = useQueryClient();
 
-    const result = useQuery<GetPostDetailPageQuery>(
-        ["GetPostDetailPage"],
-        async () => {
-            const result = await queryClient.request<
-                GetPostDetailPageQuery,
-                GetPostDetailPageQueryVariables
-            >(
-                gql`
-                    query GetPostDetailPage($postId: uuid!) {
-                        posts(where: { postId: { _eq: $postId } }) {
-                            title
-                            postId
-                            content
-                            createdAt
-                            user {
-                                avatar
-                                name
-                            }
+    const result = useQuery<GetPostDetailPageQuery>(["GetPostDetailPage"], async () => {
+        const result = await queryClient.request<
+            GetPostDetailPageQuery,
+            GetPostDetailPageQueryVariables
+        >(
+            gql`
+                query GetPostDetailPage($postId: uuid!) {
+                    posts(where: { postId: { _eq: $postId } }) {
+                        title
+                        postId
+                        content
+                        createdAt
+                        user {
+                            avatar
+                            name
                         }
                     }
-                `,
-                { postId }
-            );
+                }
+            `,
+            { postId }
+        );
 
-            return result;
-        },
-        {
-            retry: false,
-        }
-    );
+        return result;
+    });
 
     return result;
 };
