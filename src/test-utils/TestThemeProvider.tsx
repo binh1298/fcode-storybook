5529be2d2cf4f9237967faa41694b02b87d48531
Trung Kien <kien123456k@gmail.com>
2021-09-07 10:13:41 +0700

    change ReactNode props type to PropsWithChildren in TestThemeProvider HOC

--- src/test-utils/TestThemeProvider.tsx
+++ src/test-utils/TestThemeProvider.tsx
@@ -1,10 +1,10 @@
-import { ReactElement } from "react";
+import { PropsWithChildren } from "react";
 
 import { ThemeProvider } from "@material-ui/core/styles";
 
 import replyTheme from "src/theme/replyTheme";
 
-const TestThemeProvider = (props: { children: ReactElement }) => {
+const TestThemeProvider = (props: { children: PropsWithChildren<{}> }) => {
     return <ThemeProvider theme={replyTheme}>{props.children}</ThemeProvider>;
 };
 
