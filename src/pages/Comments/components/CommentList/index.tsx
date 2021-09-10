b2f352ef88422c7f5c3a91722c87b486ec26f3d0
Trung Kien <kien123456k@gmail.com>
2021-09-10 10:57:25 +0700

    replace BoxBase with GridBase in CommentList component

--- src/pages/Comments/components/CommentList/index.tsx
+++ src/pages/Comments/components/CommentList/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import { GetCommentsListQuery } from "src/generated/graphql";
 
-import BoxBase from "src/components/Boxs/BoxBase";
 import CommentItem from "src/components/Comments/CommentItem";
+import GridBase from "src/components/Grids";
 import CircularProgress from "src/components/Progress/CircularProgressBase";
 
 import useDeleteComment from "../../hooks/useDeleteComment";
@@ -32,22 +32,24 @@ const CommentList = (props: CommentListProps) => {
             {isLoading ? (
                 <CircularProgress color="secondary" />
             ) : (
-                comments.map((comment) => (
-                    <BoxBase key={comment.commentId} m={3}>
-                        <CommentItem
-                            commentId={comment.commentId}
-                            authorName={comment.user!.name}
-                            authorAvatar={comment.user?.avatar}
-                            content={comment.content}
-                            createdAt={comment.createdAt}
-                            deleteCommentHandler={deleteComment}
-                            isDeleteCommentLoading={isDeleteCommentLoading}
-                            updateCommentHandler={updateComment}
-                            isUpdateCommentLoading={isUpdateCommentLoading}
-                            validateCommentHandler={validateCommentHandler}
-                        />
-                    </BoxBase>
-                ))
+                <GridBase container spacing={3}>
+                    {comments.map((comment) => (
+                        <GridBase key={comment.commentId} item xs={12} sm={10} lg={8}>
+                            <CommentItem
+                                commentId={comment.commentId}
+                                authorName={comment.user!.name}
+                                authorAvatar={comment.user?.avatar}
+                                content={comment.content}
+                                createdAt={comment.createdAt}
+                                deleteCommentHandler={deleteComment}
+                                isDeleteCommentLoading={isDeleteCommentLoading}
+                                updateCommentHandler={updateComment}
+                                isUpdateCommentLoading={isUpdateCommentLoading}
+                                validateCommentHandler={validateCommentHandler}
+                            />
+                        </GridBase>
+                    ))}
+                </GridBase>
             )}
         </React.Fragment>
     );
