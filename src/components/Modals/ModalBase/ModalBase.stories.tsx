1e2f29baf18e9dec65830374a52bfadd043b7c45
thienduc12311 <thienduc12311@gmail.com>
2021-10-06 12:19:43 +0700

    fix modal component

--- src/components/Modals/ModalBase/ModalBase.stories.tsx
+++ src/components/Modals/ModalBase/ModalBase.stories.tsx
@@ -1,33 +1,24 @@
 import { useState } from "react";
 
-import { makeStyles } from "@material-ui/styles";
 import BoxBase from "src/components/Boxes/BoxBase";
 import ButtonBase from "src/components/Buttons/ButtonBase";
 import TypographyBase from "src/components/Typography/TypographyBase";
 
 import ModalBase, { ModalBaseProps } from ".";
 
-import { Story, Meta } from "@storybook/react";
+import { Story } from "@storybook/react";
 
 export default {
     title: "Components/Modals/ModalBase",
     component: ModalBase,
-} as Meta;
-const useStyles = makeStyles(() => ({
-    paper: {
-        position: "absolute",
-        width: 400,
-        backgroundColor: "#fff",
-        border: "2px solid #000",
-    },
-}));
+};
+
 const Template: Story<ModalBaseProps> = (args) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <BoxBase maxWidth={500}>
             <ModalBase {...args} open={isOpen} onClose={() => setIsOpen(false)}>
                 <BoxBase
-                    border
                     bgcolor="primary"
                     width={500}
                     height={300}
