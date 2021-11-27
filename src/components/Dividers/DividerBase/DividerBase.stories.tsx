6f0bb9b6e4540a3fb6f91a13efdfb41401beb92f
Trung Kien <kien123456k@gmail.com>
2021-11-28 01:20:50 +0700

    fix and refactor DividerBase to fit ListBase change

--- src/components/Dividers/DividerBase/DividerBase.stories.tsx
+++ src/components/Dividers/DividerBase/DividerBase.stories.tsx
@@ -1,37 +1,45 @@
 import DividerBase, { DividerBaseProps } from ".";
 import BoxBase from "../../Boxes/BoxBase";
+import ListItemButtonBase from "../../ListItemButtons/ListItemButtonBase";
 import ListItemTextBase from "../../ListItemTexts/ListItemTextBase";
-import ListItemButtonBase from "../../ListItems/ListItemButtonBase";
+import ListItemBase from "../../ListItems/ListItemBase";
 import ListBase from "../../Lists/ListBase";
 
 import { Story } from "@storybook/react";
 
 export default {
     title: "Components/DividerBase",
-    component: { DividerBase, ListBase, ListItemBase: ListItemButtonBase, BoxBase },
+    component: {
+        DividerBase,
+        ListBase,
+        ListItemBase,
+        ListItemButtonBase,
+        ListItemTextBase,
+        BoxBase,
+    },
 };
 
-// @kien-tran please help me check this and the ListItemButtonBase component
+const dividerListItems = [
+    { text: "Inbox", isHighlighted: true },
+    { text: "Drafts", isHighlighted: false },
+    { text: "Trash", isHighlighted: false },
+    { text: "Spam", isHighlighted: false },
+];
+
 const Template: Story<DividerBaseProps> = (args) => {
     return (
         <BoxBase width={360}>
             <ListBase>
-                <ListItemButtonBase disableGutters>
-                    <ListItemTextBase primary="Inbox" />
-                </ListItemButtonBase>
-                <DividerBase {...args} />
-                <ListItemButtonBase disableGutters>
-                    <ListItemTextBase primary="Drafts" />
-                </ListItemButtonBase>
-                <DividerBase {...args} />
-                <ListItemButtonBase disableGutters>
-                    <ListItemTextBase primary="Trash" />
-                </ListItemButtonBase>
-                <DividerBase {...args} />
-                <ListItemButtonBase disableGutters>
-                    <ListItemTextBase primary="Spam" />
-                </ListItemButtonBase>
-                <DividerBase {...args} />
+                {dividerListItems.map((item, index) => (
+                    <BoxBase key={index}>
+                        <ListItemBase disablePadding>
+                            <ListItemButtonBase>
+                                <ListItemTextBase primary={item.text} />
+                            </ListItemButtonBase>
+                        </ListItemBase>
+                        <DividerBase {...args} />
+                    </BoxBase>
+                ))}
             </ListBase>
         </BoxBase>
     );
