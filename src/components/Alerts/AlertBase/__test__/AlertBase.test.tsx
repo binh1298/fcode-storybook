97557f385a530ab71e620e1fac4b255c9c86d4cc
Trung Kien <kien123456k@gmail.com>
2021-12-10 17:36:18 +0700

    fix AlertBase component tests

--- src/components/Alerts/AlertBase/__test__/AlertBase.test.tsx
+++ src/components/Alerts/AlertBase/__test__/AlertBase.test.tsx
@@ -1,6 +1,8 @@
 import AlertBase, { AlertBaseProps } from "../";
 
 import { render, screen } from "@testing-library/react";
+import TestThemeProvider from "src/test-utils/TestThemeProvider";
+import replyTheme from "src/theme/replyTheme";
 
 describe("<AlertBase />", () => {
     const props: AlertBaseProps = {
@@ -10,10 +12,18 @@ describe("<AlertBase />", () => {
     };
 
     beforeEach(() => {
-        render(<AlertBase {...props} />);
+        render(
+            <TestThemeProvider>
+                <AlertBase {...props} />
+            </TestThemeProvider>
+        );
     });
 
     it("should exist children", () => {
+        const AlertBaseRoot = screen.getByTestId("AlertBase__root");
+
         expect(screen.getByText("Test")).toBeInTheDocument();
+        expect(AlertBaseRoot).toHaveStyle(`
+        backgroundColor: ${replyTheme.palette.success.main};`);
     });
 });
