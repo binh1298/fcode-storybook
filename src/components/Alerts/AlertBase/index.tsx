000dbe46f405adc5b7823b3726aa003fb34fe8a1
thienduc12311 <thienduc12311@gmail.com>
2021-12-21 10:26:58 +0700

    fix snackbar

--- src/components/Alerts/AlertBase/index.tsx
+++ src/components/Alerts/AlertBase/index.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 import { AlertProps as MaterialAlertProps } from "@mui/lab";
 import { Alert as MaterialAlert } from "@mui/material";
 
 export interface AlertBaseProps extends MaterialAlertProps {}
 
-const AlertBase = (props: AlertBaseProps) => {
-    return <MaterialAlert {...props} />;
-};
+const AlertBase = React.forwardRef((props: AlertBaseProps, ref: React.Ref<HTMLDivElement>) => {
+    const { variant, ...rest } = props;
+    return <MaterialAlert variant={variant || "filled"} {...rest} ref={ref} />;
+});
 
 export default AlertBase;
--- src/components/SnackBars/SnackbarBase/index.tsx
+++ src/components/SnackBars/SnackbarBase/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import {
     Snackbar as MaterialSnackbar,
     SnackbarProps as MaterialSnackbarProps,
